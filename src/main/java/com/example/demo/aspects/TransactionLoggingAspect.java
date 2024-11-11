e26be85eeb0fe738f7b66eda5cfd4e1852d83d58
Stefan Mitzkus <smitzkus@gmail.com>
2024-11-11 12:01:09 +0100

    Added @DeclarePrecedence

--- src/main/java/com/example/demo/aspects/TransactionLoggingAspect.java
+++ src/main/java/com/example/demo/aspects/TransactionLoggingAspect.java
@@ -2,12 +2,14 @@ package com.example.demo.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.DeclarePrecedence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@DeclarePrecedence("org.springframework.transaction.aspectj.AnnotationTransactionAspect, org.springframework.scheduling.aspectj.AnnotationAsyncExecutionAspect")
 public class TransactionLoggingAspect {
 
     private static final Logger LOG = LoggerFactory.getLogger(TransactionLoggingAspect.class);
